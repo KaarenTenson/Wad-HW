6272ac1967548bd79ff00f54ad5c4e474a2fbd9d
Kaaren Tenson <tensonkaaren@gmail.com>
2024-11-01 14:49:02 +0200

    like nupp töötab, tegin veits ilusmakas

--- script.js
+++ script.js
@@ -22,7 +22,10 @@ function RemovePost(indeks){
     const post = posts.children[indeks];  // Select the post by index
     posts.removeChild(post);  // Remove the post from its parent
 }
-function AddPost(tekst, profpic="icons/me.png", img=null, dateString= null){
+function AddPost(tekst, likes=0, profpic=null, img=null, dateString= null){
+    if(profpic==null){
+        profpic="icons/me.png";
+    }
     const posts = document.getElementsByClassName("keskmine")[0];
     const post = document.createElement('section');
     post.className = 'post';
@@ -55,10 +58,14 @@ function AddPost(tekst, profpic="icons/me.png", img=null, dateString= null){
     reactionSection.className = 'reaktsioon';
 
     // Create the like button image
-    const likeImage = document.createElement('img');
-    likeImage.src = 'icons/like.png';  // Use the default like icon
-    likeImage.alt = 'like';
-    reactionSection.appendChild(likeImage);
+    const likeButton = document.createElement('button');
+    likeButton.className="like_button";
+    LikeButtonEventListener(likeButton);
+    const liketext = document.createElement('p');
+    liketext.innerText =likes;
+    liketext.className="reaktsioonLikeText"
+    reactionSection.appendChild(likeButton);
+    reactionSection.appendChild(liketext);
     post.appendChild(postHeader);
     post.appendChild(content);
     if (img) {
@@ -71,6 +78,30 @@ function AddPost(tekst, profpic="icons/me.png", img=null, dateString= null){
     posts.appendChild(post);
 
 }
+function AddLikeButtonEvent(){
+    const buttons = document.getElementsByClassName("like_button");
+    Array.from(buttons).forEach(element => {
+        LikeButtonEventListener(element)
+    });
+
+}
+function LikeButtonEventListener(btn){
+    btn.addEventListener("click", function(){
+        if(this.className === "like_button"){
+            this.className="like_button_clicked";
+            sibling=this.nextElementSibling;
+            sibling.innerText=parseInt(sibling.innerText)+1; // Increase the number of likes by 1
+        }
+        else{
+            this.className="like_button";
+            sibling=this.nextElementSibling;
+            sibling.innerText=parseInt(sibling.innerText)-1;
+        } 
+    
+})}
+AddLikeButtonEvent();
 SetProfileInfo("Kaaren","ka@gmail.com", "5544353")
-RemovePost(0)
-AddPost("eheheh", "icons/download.png", null,"2014.23.34");
+//RemovePost(0)
+
+
+AddPost("Ts", null, null, null, "01.11.2024");
