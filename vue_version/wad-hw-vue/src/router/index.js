37960c4441f044e4ae21a9c9a2922529510c687b
snips66 <kippus@windowslive.com>
2024-11-25 18:51:03 +0200

    Login page almost finished

--- vue_version/wad-hw-vue/src/router/index.js
+++ vue_version/wad-hw-vue/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/MainPageView.vue')
   
+  },
+  {
+    path: '',
+    name: 'Login',
+    component: LoginView
   }
 ]
 
---
+++ vue_version/wad-hw-vue/src/views/LoginView.vue
@@ -0,0 +1,143 @@
+<script>
+export default {
+  name: "LoginView",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitLogin() {
+      console.log(`Logging in with email: ${this.email}, password: ${this.password}`);
+      this.$router.push("/about");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="login-page">
+    <div class="form">
+      <strong>Welcome to PostIt</strong>
+      <div>
+        <p>Create an account</p>
+      </div>
+      <p>or</p>
+      <p>Please log in</p>
+      <form @submit.prevent="submitLogin">
+        <input
+          type="email"
+          v-model="email"
+          required
+          placeholder="Email"
+        />
+        <br />
+        <input
+          type="password"
+          v-model="password"
+          required
+          placeholder="Password"
+        />
+        <br />
+        <input type="submit" value="Signup" />
+      </form>
+      <div>
+        <p>Forgot password</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.form {
+  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  border-radius: 10px;
+  margin-top: 20px;
+  margin-left: 20%;
+  margin-right: 20%;
+  padding-top: 10px;
+}
+
+.form div > p {
+  color: cyan;
+}
+
+#submit {
+  background-color: blue;
+  color: white;
+}
+
+form input {
+  margin: 10px;
+}
+
+.form p {
+  margin: 10px;
+}
+
+#submit:hover {
+  background-color: lightblue;
+}
+
+div + p {
+  text-decoration: underline;
+}
+
+.form p ~ p {
+  color: yellow;
+}
+
+@media (max-width: 800px) {
+  .header {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .form {
+    flex-direction: column;
+    align-items: center;
+    margin: 0%;
+  }
+
+  button {
+    width: 100%;
+  }
+}
+
+.profile-panel {
+  display: none; /* Hidden by default */
+  position: absolute;
+  top: 110px; /* Below the profile image */
+  right: 10px;
+  background-color: #f9f9f9;
+  padding: 10px;
+  border: 1px solid #ccc;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  width: 200px;
+  z-index: 1000;
+}
+
+.profile-panel h3 {
+  margin: 0;
+  font-size: 18px;
+}
+
+.profile-panel p {
+  margin: 5px 0;
+  font-size: 14px;
+}
+
+.profile-panel a {
+  margin: 5px 0;
+  font-size: 14px;
+  color: black;
+}
+
+</style>
